74d6b500e9e64969648d7a244348623b635a8677
Maxime Leszczynski <maxime.leszczynski@laposte.net>
2020-04-24 11:08:26 +0200

    Mise en place de Webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+  watch: true,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    open: true
+  }
+}
